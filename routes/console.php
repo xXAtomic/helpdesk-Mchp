612dc27bdf58ecd444ed853f0726145ef2a168c3
Cristopher Adones <cris.adones@gmail.com>
2026-04-09 22:33:19 -0400

    Chore: Automate email fetching in console.php

---
+++ routes/console.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote')->hourly();
+
+// Automatización de lectura de correos de soporte cada 5 minutos
+Schedule::command('gravity:fetch-emails')->everyFiveMinutes();
